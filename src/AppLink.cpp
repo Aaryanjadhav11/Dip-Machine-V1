77af4542542a93e4cbf6220076bdb61cc2be2eef
Aaryan <aaryanjadhav9007@gmail.com>
2024-07-25 01:47:05 +0530

    added OTA updating

--- src/AppLink.cpp
+++ src/AppLink.cpp
@@ -32,11 +32,12 @@ void updateWiFi(const char* ssid, const char* pass);
 // ========================== Main: appLinkInit =========================
 void appLinkInit(void * parameters) {
     HandleWiFi();
+    ArduinoOTA.begin();
+    MDNS.begin("DipMachine");
+
     while (true) {
         vTaskDelay(10);
-        int count;
-        Network* network = scanNetworks(count);
-        delete[] network;
+        ArduinoOTA.handle();
     }
 } // appLinkInit
 
@@ -83,7 +84,11 @@ void updateWiFi(const char* ssid, const char* pass){
     bool setPass = preferences.putString("password", pass);
     Serial.printf("[updateWiFi] Stored SSID: %s, Password: %s\n", ssid, pass);
     preferences.end();
-    HandleWiFi();
+
+    if (setSSID && setPass) {
+        WiFi.disconnect();
+        HandleWiFi();
+    }
 } // updateWiFi
 
 void HandleWiFi() {
@@ -93,13 +98,12 @@ void HandleWiFi() {
     String password = preferences.getString("password", "");
     preferences.end();
 
-    Serial.printf("[HandleWiFi] Attempting to connect to SSID: %s\n", ssid.c_str());
+    Serial.printf("[HandleWiFi] Attempting to connect to SSID: %s...", ssid.c_str());
     WiFi.setHostname("DipMachine");
     WiFi.begin(ssid.c_str(), password.c_str());
 
     unsigned long start = millis();
     // Attempt to connect to WiFi
-    Serial.print("[HandleWiFi] Connecting");
     while (WiFi.status() != WL_CONNECTED && millis() - start < 5000) {
         Serial.print(".");
         digitalWrite(BUILTIN_LED, HIGH);
@@ -128,3 +132,32 @@ void HandleWiFi() {
         Serial.println(WiFi.softAPIP());
     }
 } // HandleWiFi
+
+// ========================| OTA function |=================================
+void initOTA(){
+  Serial.println("Initializing OTA");
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+} // initOTA
+
